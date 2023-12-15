ccf2b21318853a6f84ba14a04d8c7155db71b198
AMRISH SHARMA <127399235+amrish1255@users.noreply.github.com>
2023-12-15 15:46:04 +0530

    Add files via upload

---
+++ L1.java
@@ -0,0 +1,25 @@
+package Recursion_withSubsequence;
+
+public class L1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		Toss(n, "");
+
+	}
+
+	public static void Toss(int n, String ans) {
+		if (n == 0) {
+			System.out.print (ans + " ");
+			return;
+		}
+		if (ans.length() == 0 || ans.charAt(ans.length() - 1) != 'H') 
+			{
+				Toss(n - 1, ans + "H");
+			}
+
+			Toss(n - 1, ans + "T");
+		}
+	}
+
